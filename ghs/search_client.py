8fdc4de33d1731a0b46573aee8ea415436365021
Vinicius Arcanjo <vindasil@fiu.edu>
2022-02-22 17:41:25 -0300

    Added paginated search support

--- ghs/search_client.py
+++ ghs/search_client.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import asyncio
 import json
+import math
 from collections import Counter
 from typing import List, Optional
 
@@ -11,7 +13,7 @@ from .base import base_url, headers
 
 
 def _build_args(dict_args, kv_sep="=", arg_sep="&") -> str:
-    """Build query search args."""
+    """Build URL args."""
     return arg_sep.join([f"{k}{kv_sep}{v}" for k, v in dict_args.items()])
 
 
@@ -52,27 +54,58 @@ def _map_search_item(item: dict) -> dict:
     }
 
 
-async def _search(**kwargs) -> dict:
+async def _search(query_expr: str, page=1, per_page=100) -> dict:
     """Search issues and PRs."""
-    # TODO handle pagination
-    url_args = _build_args(kwargs, kv_sep=":", arg_sep="+")
-    url = f"{base_url()}/search/issues?q={url_args}"
+    url_args = "+".join(query_expr.split(" "))
+    page_args = _build_args({"page": page, "per_page": per_page}, "=", "&")
+    url = f"{base_url()}/search/issues?q={url_args}&{page_args}"
     async with httpx.AsyncClient() as client:
         r = await client.get(url, headers=headers())
         r.raise_for_status()
         return r.json()
 
 
-async def search(**kwargs) -> str:
-    """Search issues and PRs."""
-    response = await _search(**kwargs)
-    items = [_map_search_item(item) for item in response.get("items")]
+def _map_search_response(items: List[dict]) -> str:
+    """Map search reponse."""
     items = sorted(
         items, key=lambda x: _priority_to_int(x.get("priority")), reverse=True
     )
     counter = Counter([item.get("state") for item in items])
     open_items = counter.get("open", 0)
     closed_items = counter.get("closed", 0)
-    progress = closed_items / open_items if open_items else 0
-    stats = {"open": open_items, "closed": closed_items, "progress": progress}
-    return json.dumps({"items": items, "stats": stats}, sort_keys=True, indent=4)
+    progress = closed_items / len(items) if len(items) else 0
+    stats = {
+        "open": open_items,
+        "closed": closed_items,
+        "progress": progress,
+    }
+    return json.dumps(
+        {
+            "items": items,
+            "stats": stats,
+            "partial_results": True if len(items) == 1000 else False,
+        },
+        sort_keys=True,
+        indent=4,
+    )
+
+
+async def search(query_expr: str) -> str:
+    """Search issues and PRs, the query_expr is an expression that GitHub search
+    supports such as 'org:some_org label:some_label'."""
+    response = await _search(query_expr)
+    items = [_map_search_item(item) for item in response.get("items")]
+    per_page = 100
+
+    remaining_pages = min(
+        math.ceil((int(response.get("total_count")) - len(items)) / per_page), 9
+    )
+    print(remaining_pages)
+    coros = [
+        _search(query_expr, page=page, per_page=per_page)
+        for page in range(2, 2 + remaining_pages)
+    ]
+    for response in await asyncio.gather(*coros):
+        for item in response.get("items"):
+            items.append(_map_search_item(item))
+    return _map_search_response(items)
